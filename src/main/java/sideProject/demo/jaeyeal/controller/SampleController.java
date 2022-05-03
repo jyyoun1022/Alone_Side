845f6198ebfb584f2036784ae31d77236d36a348
younjaeyeal <jyyoun1021@gmail.com>
2022-05-03 23:35:31 +0900

    ajax테스트

--- src/main/java/sideProject/demo/jaeyeal/controller/SampleController.java
+++ src/main/java/sideProject/demo/jaeyeal/controller/SampleController.java
@@ -1,19 +1,18 @@
 package sideProject.demo.jaeyeal.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Log4j2
 @RequestMapping("/test")
-@RestController
+@Controller
 public class SampleController {
 
-        @GetMapping(value="")
-        public String test() {
+        @GetMapping("/test")
+        public void test() {
             log.info("test test test test");
-            return "TEST COMPLETE?? jayeal test?";
         }
 
 }
